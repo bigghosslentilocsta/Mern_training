a2a54506caf1364de1d4e7e23052f8c292a15bb4
Pavan Kumar <pavankumar111205@gmail.com>
2026-01-17 19:31:22 +0530

    updating all assignments

---
+++ array operations/Operators.js
@@ -0,0 +1,22 @@
+let totalAmount = 0;
+
+//add 500 to total
+
+totalAmount += 500;
+console.log("Total Amount after adding 500:", totalAmount);
+
+//add 1200 to total
+
+totalAmount += 1200;
+console.log("Total Amount after adding 1200:", totalAmount);
+
+//apply discount of 200
+
+totalAmount -= 200;
+console.log("Total Amount after applying discount of 200:", totalAmount);
+
+//add 18% gst
+totalAmount += totalAmount * 0.18;
+console.log("Total Amount after adding 18% GST:", totalAmount);
+
+console.log("Final Total Amount:", totalAmount);
